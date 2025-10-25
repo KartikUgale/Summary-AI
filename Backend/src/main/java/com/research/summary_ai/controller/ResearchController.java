851f63af7e6183db79853bd876acb00f1ddbb06b
KartikUgale <kartikugale822@gmail.com>
2025-10-25 16:38:36 +0530

    initial Commit *

---
+++ Backend/src/main/java/com/research/summary_ai/controller/ResearchController.java
@@ -0,0 +1,23 @@
+package com.research.summary_ai.controller;
+
+
+import com.research.summary_ai.entity.ResearchRequest;
+import com.research.summary_ai.service.ResearchService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class ResearchController {
+
+    private final ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request) {
+        String result = researchService.processContent(request);
+        return ResponseEntity.ok(result);
+    }
+}
